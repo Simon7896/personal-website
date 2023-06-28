3de262418db63f6f64ea83b4162e8887476ee496
simonyu <simonyu180@gmail.com>
2023-06-27 19:48:09 -0700

    Refactored top bar into single component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,44 +1,21 @@
-import Image from 'next/image'
 
-type TopBarProps = {
-  bgcolor: string;
-  children: React.ReactNode;
+const TopBar: React.FunctionComponent = () => {
+  return (
+    <ul className="flex flex-row justify-evenly w-full">
+      <li className="basis-1/6 m-5 p-5">Home</li>
+      <li className="basis-1/6 m-5 p-5">Projects</li>
+      <li className="basis-1/6 m-5 p-5">About</li>
+      <li className="basis-1/6 m-5 p-5">Contacts</li>
+    </ul>
+  )
 }
-
-type TopBarItemProps = {
-  name: string;
-  onClick(): void;
-}
-
-const TopBar: React.FunctionComponent<TopBarProps> = ({
-  children, ...props}) => {
-    const {bgcolor} = props;
-
-    return (
-      <ul>
-        {children}
-      </ul>
-    );
-};
-
-const TopBarItem: React.FunctionComponent<TopBarItemProps> = ({
-  ...props}) => {
-    const {name, onClick} = props;
-
-    return (
-      <li>{name}</li>
-    )
-  }
-
 
 export default function App() {
   return (
     <main>
-      <TopBar bgcolor='white'>
-        <TopBarItem name='About' onClick={()=>{}}/>
-        <TopBarItem name='Projects' onClick={()=>{}}/>
-        <TopBarItem name='Contacts' onClick={()=>{}}/>
-      </TopBar>
+      <div className="w-full">
+        <TopBar/>
+      </div>
     </main>
   )
 }
