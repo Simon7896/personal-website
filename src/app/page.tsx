fccdce41326b244a5f8ad4bdbfe3b670510bd34a
simonyu <simonyu180@gmail.com>
2023-06-27 20:42:52 -0700

    Moved top bar to layout file, added new pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,7 @@
-
-const TopBar: React.FunctionComponent = () => {
-  return (
-    <ul className="flex flex-row justify-evenly w-full">
-      <li className="basis-1/6 m-5 p-5">Home</li>
-      <li className="basis-1/6 m-5 p-5">Projects</li>
-      <li className="basis-1/6 m-5 p-5">About</li>
-      <li className="basis-1/6 m-5 p-5">Contacts</li>
-    </ul>
-  )
-}
-
 export default function App() {
   return (
-    <main>
-      <div className="w-full">
-        <TopBar/>
-      </div>
-    </main>
+    <div>
+      
+    </div>
   )
 }
