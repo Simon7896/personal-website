c1a96a6030889ea34c071b7340d87f03be7100c7
simonyu <simonyu180@gmail.com>
2023-06-29 13:59:12 -0700

    Refactored banners into components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,54 @@
 import Link from "next/link"
 import {MdArrowForward} from "react-icons/md"
 
+type SectionProps = {
+  bgcolor: string;
+  title?: string;
+  children: React.ReactNode;
+}
+
+const SectionBox = ({ bgcolor, children }: SectionProps) => {
+  return (
+    <div className={"grow basis-1/2 w-full py-48 self-center flex flex-col justify-center " + bgcolor}>
+        { children }
+    </div>
+  )
+}
+
+const Section = ({ bgcolor, title, children}: SectionProps) => {
+  return (
+    <SectionBox bgcolor={ bgcolor }>
+      <h1 className="text-4xl text-center">{ title }</h1>
+      { children }
+    </SectionBox>
+  )
+}
+
+type RoundArrowTextButtonProps = {
+  href: string;
+  text: string;
+}
+
+const RoundArrowTextButton = ({ href, text }: RoundArrowTextButtonProps) => {
+  return (
+    <Link href={ href } className="flex flex-row self-center justify-center items-center p-3 m-5 rounded-full bg-slate-700 hover:bg-slate-600">
+      <p className="text-2xl text-center mx-2">{ text }</p>
+      <MdArrowForward/>
+    </Link>
+  )
+}
+
 export default function App() {
   return (
-    <div className="flex flex-col items-stretch space-evenly w-full h-screen">
-      <div className="basis-1/2 w-full self-center flex flex-col justify-center bg-slate-700">
-        <h1 className="text-4xl text-center">Welcome!</h1>
-        <h2 className="text-2xl text-center">I Hope You Have A Nice Day!</h2>
-      </div>
-      <div className="basis-1/2 w-full self-center flex flex-col justify-center bg-slate-800">
-        <h1 className="text-4xl text-center m-5">Check Out My Projects!</h1>
-        <Link href="/projects" className="flex flex-row self-center justify-center items-center p-3 m-5 rounded-full bg-slate-700 hover:bg-slate-600">
-          <p className="text-2xl text-center mx-2">Projects </p>
-          <MdArrowForward/>
-        </Link>
-      </div>
+    <div className="flex flex-col items-stretch space-evenly w-full h-fit">
+      <Section bgcolor="bg-slate-700" title="Welcome!">
+      </Section>
+      <Section bgcolor="bg-slate-800" title="Check Out My Projects!">
+        <RoundArrowTextButton href="/projects" text="Projects"/>
+      </Section>
+      <Section bgcolor="bg-slate-500" title="Contact Me!">
+        <RoundArrowTextButton href="/contacts" text="Contacts"/>
+      </Section>
     </div>
   )
 }
