04ea33c82fd40ff55272e33c65724b8e6553ca7d
simonyu <simonyu180@gmail.com>
2023-06-29 12:48:34 -0700

    Added clickable card component and grid container

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,5 +1,49 @@
+import Link from "next/link"
+
+type Children = {
+    children: React.ReactNode;
+}
+
+type CardProps = {
+    href: string;
+    title: string;
+    desc: string;
+    img: string;
+}
+
+const CardContainer = ({ children }: Children) => {
+    return (
+        <div className="p-4 rounded-lg drop-shadow-lg bg-slate-800 hover:bg-slate-600">
+            { children }
+        </div>
+    )
+}
+
+const Card = ({href, title, desc, img}: CardProps) => {
+    return (
+        <CardContainer>
+            <Link href={ href }>
+                <div className="flex flex-col">
+                    <div>
+                        <img src={ img }/>
+                    </div>
+                    <h1 className="mt-2 uppercase text-lg font-medium">{ title }</h1>
+                    <p className="mt-2 font-medium">{ desc }</p>
+                </div>
+            </Link>
+        </CardContainer>
+    )
+}
+
 export default function Page() {
     return (
-        <h1>These are the projects that I have worked on:</h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-4 gap-4 m-8">
+            <Card href="/" title="Test Title" desc="This is a Test Description for the card component which works very well" img="/vercel.svg"/>
+            <Card href="/" title="Test Title" desc="This is a Test Description for the card component" img="/next.svg"/>
+            <Card href="/" title="Test Title" desc="This is a Test Description for the card component" img="/"/>
+            <Card href="/" title="Test Title" desc="This is a Test Description for the card component" img="/"/>
+            <Card href="/" title="Test Title" desc="This is a Test Description for the card component" img="/"/>
+            <Card href="/" title="Test Title" desc="This is a Test Description for the card component" img="/"/>
+        </div>
     )
 }
