313e748ef36993a25b91219160725fc40d132663
simonyu <simonyu180@gmail.com>
2023-06-30 12:26:05 -0700

    Added Banner

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
     return (
-        <div>
-            <h1>About Me</h1>
+        <div className="p-64 w-full flex flex-col justify-center bg-slate-700">
+            <h1 className="text-4xl text-center">About Me</h1>
         </div>
     )
 }
