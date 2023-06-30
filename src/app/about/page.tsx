cda7ccc0c58752c6122398c80a4eea3ef841d592
simonyu <simonyu180@gmail.com>
2023-06-30 14:57:11 -0700

    Changed background color

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,6 @@
 export default function Page() {
     return (
-        <div className="p-64 w-full flex flex-col justify-center bg-slate-700">
+        <div className="p-64 w-full flex flex-col justify-center bg-slate-200 dark:bg-slate-700">
             <h1 className="text-4xl text-center">About Me</h1>
         </div>
     )
