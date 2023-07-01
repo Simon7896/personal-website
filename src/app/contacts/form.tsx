0ec1753d73f281f7a989d98d76cd520f548c4394
simonyu <simonyu180@gmail.com>
2023-06-30 21:34:35 -0700

    Implemented message feature

--- src/app/contacts/form.tsx
+++ src/app/contacts/form.tsx
@@ -1,32 +1,54 @@
 'use client'
 
-import { FormEventHandler } from "react";
-
-type FormInputs = {
-    email: string;
-    message: string;
-}
+import { useState } from "react";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import sendMsg from "./sendMsg";
+import { Toast } from "react-toastify/dist/components";
 
 export default function Form() {
-    const handleSubmit: FormEventHandler<HTMLFormElement> = (e) => {
-        e.preventDefault();
+    
+    const [message, setMessage] = useState('');
+    const [name, setName] = useState('');
 
-        const form = e.currentTarget;
-        const formData = new FormData(e.currentTarget);
-
-        const formJson = Object.fromEntries(formData.entries());
-        console.log(formJson);
+    const resetForm = () => {
+        setMessage('');
+        setName('');
     }
+
+    const send = (formData: FormData) => {
+        sendMsg(formData)
+        resetForm()
+        notify()
+    }
+
+    const notify = () => toast("Message Sent!");
 
     return (
         <div>
-            <form method="post" onSubmit={handleSubmit}>
-                <h1 className="text-4xl text-center">Send A Message</h1>
+            <form action={send}>
+                <h1 className="p-5 text-4xl text-center">Send A Message</h1>
+                <label className="flex justify-center">
+                    Name:<input 
+                        className="mx-5 text-black" 
+                        name="name"
+                        value={name}
+                        onChange={(e)=>{setName(e.target.value)}}
+                        type="text"
+                    />
+                </label>
                 <label className="m-5 flex justify-center text-black">
-                    <textarea name="message" rows={10} cols={40} />
+                    <textarea 
+                        name="message"
+                        value={message}
+                        onChange={(e)=>setMessage(e.target.value)}
+                        rows={10} 
+                        cols={40} 
+                    />
                 </label>
                 <button className="p-4 rounded-full bg-slate-500 text-xl" type="submit">Send</button>
             </form>
+            <ToastContainer/>
         </div>
     )
 }
