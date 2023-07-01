743f6616f1f78b1320f8dd5b12ea5ee4dee8cf0b
simonyu <simonyu180@gmail.com>
2023-06-30 21:33:57 -0700

    Implemented server side component for sending msg

---
+++ src/app/contacts/sendMsg.tsx
@@ -0,0 +1,29 @@
+'use server'
+
+import nodemailer from "nodemailer"
+
+export default async function sendMsg(formData: FormData) {
+    'use server'
+
+    const formJson = Object.fromEntries(formData);
+
+    const transporter = nodemailer.createTransport({
+        port: 465,
+        host: "smtp.gmail.com",
+        auth: {
+            user: 's47026454@gmail.com',
+            pass: process.env.password
+        },
+        secure: true,
+    })
+
+    const info = await transporter.sendMail({
+        from: "<s47026454@gmail.com>",
+        to: "s47026454@gmail.com",
+        subject: `Message From ${formJson["name"].toString()}`,
+        text: formJson["message"].toString(),
+        html: `<div><p>${formJson["message"]}<p></div>`
+    });
+
+    console.log("Message sent: %s", info.messageId);
+}
