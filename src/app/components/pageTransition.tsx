043928c3e487966420a1b73fcff7b6ac19c07cfb
simonyu <simonyu180@gmail.com>
2023-07-11 16:02:14 -0700

    Fixed bug that kept page position after transition

--- src/app/components/pageTransition.tsx
+++ src/app/components/pageTransition.tsx
@@ -1,19 +1,27 @@
 'use client'
 
 import { AnimatePresence, motion } from "framer-motion"
+import { useEffect } from "react";
 
 type PageTransitionProps = {
     children: React.ReactNode;
 }
 
 const PageTransition = ({children}: PageTransitionProps) => {
+
+    useEffect(() => { // Temporary fix for page transition exit bug
+        window.scrollTo(0, 0)
+    }, []);
+
     return (
         <AnimatePresence
-            mode="popLayout"
+            mode="wait"
+            onExitComplete={() => window.scrollTo(0, 0)} // This doesn't work at the moment
         >
             <motion.div
                 initial={{ opacity:0, y: '50%' }}
                 animate={{ opacity:1, y: 0 }}
+                exit={{ opacity:0, y: '-50%' }} // This doesn't work at the moment
                 transition={{ease: "easeInOut", duration: 0.5}}
             >
                 {children}
