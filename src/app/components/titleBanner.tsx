7253a72d9d104bb2a301ae8524170f494f8e5273
simonyu <simonyu180@gmail.com>
2023-07-11 15:25:50 -0700

    Created TitleBanner component

---
+++ src/app/components/titleBanner.tsx
@@ -0,0 +1,12 @@
+type TitleBannerProps = {
+    name: string;
+    bgcolor: string;
+}
+
+const TitleBanner = ({name, bgcolor}: TitleBannerProps) => {
+    return (
+        <div className={`py-64 w-full flex flex-col justify-center ${bgcolor}`}>
+            <h1 className="text-4xl text-center">{ name }</h1>
+        </div>
+    )
+}
