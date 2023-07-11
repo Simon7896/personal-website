45ad81aaeea9b1747b38e6197c4e465b9eab728e
simonyu <simonyu180@gmail.com>
2023-07-11 13:22:45 -0700

    Fixed page sizing on smaller devices

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -13,7 +13,7 @@ type ContactItemProps = {
 
 const ContactItem = ({ text, href, children }: ContactItemProps) => {
     return (
-        <div className="flex flex-row justify-center p-8 hover:underline">
+        <div className="flex flex-row justify-center p-16 hover:underline hover:bg-slate-600 hover:animate-pulse">
             <div className="mx-2">
                 { children }
             </div>
@@ -27,16 +27,16 @@ const ContactItem = ({ text, href, children }: ContactItemProps) => {
 export default function Page() {
     return (
         <div className="flex flex-col">
-            <div className="p-64 w-full flex flex-col justify-center bg-slate-700">
+            <div className="py-64 w-full flex flex-col justify-center bg-slate-700">
                 <h1 className="text-4xl text-center">Contacts</h1>
             </div>
-            <div className="p-8 grid grid-cols-1 lg:grid-cols-2 text-center text-3xl bg-slate-500">
+            <div className="grid grid-cols-1 lg:grid-cols-2 text-center text-3xl bg-slate-500">
                 <ContactItem href="mailto:simonyu180@gmail.com" text="simonyu180@gmail.com"><MdEmail/></ContactItem>
                 <ContactItem text="Linkedin" href="https://www.linkedin.com/in/simon-yu-a9053122b"><FaLinkedin/></ContactItem>
                 <ContactItem href="https://discordapp.com/users/337459814995197952" text="Discord"><FaDiscord/></ContactItem>
                 <ContactItem text="Instagram" href="https://www.instagram.com/meepmoopz/"><FaInstagram/></ContactItem>
             </div>
-            <div className="p-64 w-full flex flex-col justify-center bg-slate-800 text-center">
+            <div className="py-10 w-full flex flex-row justify-center bg-slate-800">
                 <Form></Form>
             </div>
         </div>
