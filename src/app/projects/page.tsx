f291d771a67632431fa40a6ff5d1adbcae967b1b
simonyu <simonyu180@gmail.com>
2023-07-14 15:33:59 -0700

    Refactored Card into component

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -2,46 +2,15 @@ import Link from "next/link"
 import PageTransition from "../components/pageTransition";
 import TitleBanner from "../components/titleBanner";
 import { FaGithub, FaExternalLinkAlt } from "react-icons/fa";
+import { CardGridContainer, Card } from "../components/card";
 
-type Children = {
-    children: React.ReactNode;
-}
-
-type CardProps = {
-    href: string;
-    title: string;
-    desc: string;
-    children?: React.ReactNode;
-}
-
-const CardGridContainer = ({ children }: Children) => {
-    return (
-        <div className="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 gap-4 m-8">
-            {children}
-        </div>
-    )
-}
-
-const CardBox = ({ children }: Children) => {
-    return (
-        <div className="flex flex-col p-4 rounded-lg drop-shadow-lg bg-slate-800 hover:bg-slate-600">
-            {children}
-        </div>
-    )
-}
-
-const Card = ({ href, title, desc, children }: CardProps) => {
-    return (
-        <CardBox>
-            <Link href={href}>
-                <div className="flex flex-col">
-                    <h1 className="my-1 text-lg font-medium">{title}</h1>
-                    <p className="my-1 font-medium">{desc}</p>
-                </div>
-            </Link>
-            {children}
-        </CardBox>
-    )
+type ProjectRepo = {
+    name: string;
+    description: string;
+    avatar_url: string;
+    html_url: string;
+    homepage: string;
+    size: string;
 }
 
 async function getRepoData() {
@@ -58,15 +27,6 @@ async function getRepoData() {
     }
    
     return res.json()
-}
-
-type ProjectRepo = {
-    name: string;
-    description: string;
-    avatar_url: string;
-    html_url: string;
-    homepage: string;
-    size: string;
 }
 
 export default async function Page() {
