c047016e9489ccbec323f85bee33e10168e322d0
simonyu <simonyu180@gmail.com>
2023-07-14 15:26:54 -0700

    Added Github repo api fetching

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,7 +1,8 @@
 import Link from "next/link"
-import Image from "next/image";
 import PageTransition from "../components/pageTransition";
 import TitleBanner from "../components/titleBanner";
+import { IconContext } from "react-icons";
+import { FaGithub, FaExternalLinkAlt } from "react-icons/fa";
 
 type Children = {
     children: React.ReactNode;
@@ -11,7 +12,7 @@ type CardProps = {
     href: string;
     title: string;
     desc: string;
-    img: string;
+    children?: React.ReactNode;
 }
 
 const CardGridContainer = ({ children }: Children) => {
@@ -24,35 +25,79 @@ const CardGridContainer = ({ children }: Children) => {
 
 const CardBox = ({ children }: Children) => {
     return (
-        <div className="p-4 rounded-lg drop-shadow-lg bg-slate-800 hover:bg-slate-600">
+        <div className="flex flex-col p-4 rounded-lg drop-shadow-lg bg-slate-800 hover:bg-slate-600">
             {children}
         </div>
     )
 }
 
-const Card = ({ href, title, desc, img }: CardProps) => {
+const Card = ({ href, title, desc, children }: CardProps) => {
     return (
         <CardBox>
             <Link href={href}>
                 <div className="flex flex-col">
-                    <div>
-                        <Image src={img} width="500" height="500" alt="Project Logo"/>
-                    </div>
-                    <h1 className="mt-2 uppercase text-lg font-medium">{title}</h1>
-                    <p className="mt-2 font-medium">{desc}</p>
+                    <h1 className="my-1 text-lg font-medium">{title}</h1>
+                    <p className="my-1 font-medium">{desc}</p>
                 </div>
             </Link>
+            {children}
         </CardBox>
     )
 }
 
-export default function Page() {
+async function getRepoData() {
+    const res = await fetch('https://api.github.com/users/simon7896/repos', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `token ${process.env.GITHUB_TOKEN}`
+        }
+    })
+    
+    if (!res.ok) {
+      throw new Error('Failed to fetch data')
+    }
+   
+    return res.json()
+}
+
+type ProjectRepo = {
+    name: string;
+    description: string;
+    avatar_url: string;
+    html_url: string;
+    homepage: string;
+    size: string;
+}
+
+export default async function Page() {
+    var projectRepoJson = await getRepoData();
+
+    projectRepoJson = projectRepoJson.sort((a: ProjectRepo, b: ProjectRepo) => {
+        if (a.size > b.size) { // Sort by size
+            return -1;
+        }
+    });
+
     return (
         <PageTransition>
             <div className="block">
                 <TitleBanner name="Projects" bgcolor="bg-slate-700"></TitleBanner>
                 <CardGridContainer>
-                    <Card href="/" title="Test Title" desc="This is a Test Description for the card component which works very well" img="/vercel.svg" />
+                { projectRepoJson.map((repo: ProjectRepo) => {
+                    return (
+                        <Card href={repo.html_url} title={repo.name.replaceAll('-', ' ')} desc={repo.description}>
+                            <div className="my-2 flex flex-row">
+                                <Link href={repo.html_url} className=""><FaGithub size={28}/></Link>
+                                { repo.homepage === null || repo.homepage === '' ? null : 
+                                    <Link href={repo.homepage}>
+                                        <FaExternalLinkAlt size={28}/>
+                                    </Link>
+                                }
+                            </div>
+                        </Card>
+                    )    
+                })}
                 </CardGridContainer>
             </div>
         </PageTransition>
