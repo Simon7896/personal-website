d686258ad499acb6ebbfca9536cd021ee8fe3615
simonyu <simonyu180@gmail.com>
2023-07-17 14:08:33 -0700

    Changed top bar light mode colours

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,13 +12,13 @@ type TopBarLinkProps = {
 
 const TopBarLink = ({href, children}: TopBarLinkProps) => {
   return (
-    <Link className='basis-1/6 py-5 flex-grow bg-slate-400 bg-transparent hover:bg-slate-600' href={href}>{children}</Link>
+    <Link className='basis-1/6 py-5 flex-grow bg-slate-50 dark:bg-slate-900 bg-transparent hover:bg-slate-600' href={href}>{children}</Link>
   )
 }
 
 const TopBar = () => {
   return (
-    <div className="sticky z-50 top-0 flex flex-row content-center justify-evenly items-stretch text-center w-full bg-slate-400 dark:bg-slate-900">
+    <div className="sticky z-50 top-0 flex flex-row content-center justify-evenly items-stretch text-center w-full bg-slate-50 dark:bg-slate-900">
       <TopBarLink href="/"><p>Home</p></TopBarLink>
       <TopBarLink href="/projects"><p>Projects</p></TopBarLink>
       <TopBarLink href="/contacts"><p>Contacts</p></TopBarLink>
