d10e51d940399b7dee56266d010e06eb39802dc9
simonyu <simonyu180@gmail.com>
2023-07-17 13:42:33 -0700

    Changed font sizing on card component

--- src/app/components/card.tsx
+++ src/app/components/card.tsx
@@ -1,27 +1,29 @@
 import Link from 'next/link';
 
-type Children = {
-    children: React.ReactNode;
+type CardGridContainerProps = {
+    className?: string;
+    children?: React.ReactNode;
 }
 
 type CardProps = {
+    className?: string;
     href: string;
     title: string;
     desc: string;
     children?: React.ReactNode;
 }
 
-const CardGridContainer = ({ children }: Children) => {
+const CardGridContainer = ({ className, children }: CardGridContainerProps) => {
     return (
-        <div className="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 gap-4 p-8 bg-slate-600">
+        <div className={`grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 gap-4 p-8 bg-slate-600 ${className}`}>
             {children}
         </div>
     )
 }
 
-const CardBox = ({ children }: Children) => {
+const CardBox = ({ className, children }: CardGridContainerProps) => {
     return (
-        <div className="flex flex-col p-4 rounded-lg drop-shadow-lg bg-slate-900 hover:bg-slate-700">
+        <div className={`flex flex-col p-4 rounded-lg drop-shadow-lg bg-slate-900 hover:bg-slate-700 ${className}`}>
             {children}
         </div>
     )
@@ -32,8 +34,8 @@ const Card = ({ href, title, desc, children }: CardProps) => {
         <CardBox>
             <Link href={href}>
                 <div className="flex flex-col">
-                    <h1 className="my-1 text-lg font-medium">{title}</h1>
-                    <p className="my-1 font-medium">{desc}</p>
+                    <p className="my-1 text-2xl font-bold">{title}</p>
+                    <p className="my-1 text-lg">{desc}</p>
                 </div>
             </Link>
             {children}
